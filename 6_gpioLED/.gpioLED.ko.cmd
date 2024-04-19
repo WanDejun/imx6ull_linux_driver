a9a22a07e02a93d63de1fb860b2ccf50aa8fe3cb
WanDejun <wandejun666@gmail.com>
2024-04-20 00:11:33 +0800

    add 6_gpioLED & 5_dtsLED

--- 6_gpioLED/.gpioLED.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wandejun/linux/imx6ull/linux_driver/6_gpioLED/gpioLED.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/wandejun/linux/imx6ull/linux_driver/6_gpioLED/gpioLED.ko /home/wandejun/linux/imx6ull/linux_driver/6_gpioLED/gpioLED.o /home/wandejun/linux/imx6ull/linux_driver/6_gpioLED/gpioLED.mod.o
--- 6_gpioLED/.tmp_versions/gpioLED.mod
+++
@@ -1,2 +0,0 @@
-/home/wandejun/linux/imx6ull/linux_driver/6_gpioLED/gpioLED.ko
-/home/wandejun/linux/imx6ull/linux_driver/6_gpioLED/gpioLED.o
